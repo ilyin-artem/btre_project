743cfa4b72db726a8a48edd21f66becd52d22620
Artyom Ilyin <ilyin@infostorm.ru>
2022-02-19 20:00:56 +0300

    final

---
+++ pages/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+
+	path('', views.index, name='index'),
+	path('about', views.about, name='about'),
+
+	path('register', views.register, name='register'),
+	path('login', views.login, name='login'),
+]
